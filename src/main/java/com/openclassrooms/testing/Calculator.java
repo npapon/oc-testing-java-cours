f943cf26476ba3b820943032f1509d9d12f20227
Papoune <npapon@live.fr>
2020-08-26 07:35:53 +0200

    commit nico

--- src/main/java/com/openclassrooms/testing/Calculator.java
+++ src/main/java/com/openclassrooms/testing/Calculator.java
@@ -2,8 +2,23 @@ package com.openclassrooms.testing;
 
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a + b;
-	}
+    public int add( int a, int b ) {
+        return a + b;
+    }
+
+    public int multiply( int a, int b ) {
+        // TODO Auto-generated method stub
+        return a * b;
+    }
+
+    public void longCalcul() {
+
+        try {
+            Thread.sleep( 500 );
+        } catch ( InterruptedException e ) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -1,23 +1,103 @@
 package com.openclassrooms.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-class CalculatorTest {
+class CalcuslatorTest {
 
-	@Test
-	void testAddTwoPositiveNumbers() {
-		// Arrange
-		int a = 2;
-		int b = 3;
-		Calculator calculator = new Calculator();
+    private Calculator     calculator;
+    private int            numeroTest;
+    private static Instant debut;
 
-		// Act
-		int somme = calculator.add(a, b);
+    @BeforeEach
+    public void initCalculatorTest() {
+        calculator = new Calculator();
+    }
 
-		// Assert
-		assertEquals(5, somme);
-	}
+    @AfterEach
+    public void endTest() {
+        System.out.println( "fin test " + numeroTest );
+    }
+
+    @BeforeAll
+    public static void initTemps() {
+        System.out.println( "Lancé avant tous les tests" );
+        debut = Instant.now();
+
+    }
+
+    @AfterAll
+    public static void dureeTest() {
+        System.out.println( "Lancé après tous les tests\"" );
+        Instant fin = Instant.now();
+        long dureeTest = Duration.between( debut, fin ).toMillis();
+        System.out.println( MessageFormat.format( "Durée : {0} ms", dureeTest ) );
+
+    }
+
+    public void setNumeroTest( int numeroTest ) {
+        this.numeroTest = numeroTest;
+    }
+
+    @Test
+    public void testAddTwoPositiveNumbers() {
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        int somme = calculator.add( a, b );
+        setNumeroTest( 1 );
+
+        // Assert
+        assertEquals( 5, somme );
+    }
+
+    @Test
+    public void multiplyTwoPositiveNumbers() {
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        int produit = calculator.multiply( a, b );
+        setNumeroTest( 2 );
+
+        // Assert
+        assertEquals( 6, produit );
+    }
+
+    @ParameterizedTest( name = "un nombre * 0 est toujours égal à 0" )
+    @ValueSource( ints = { 1, 2, 1000 } )
+    public void multiplieParZero( int argument ) {
+        int produit = calculator.multiply( argument, 0 );
+        assertEquals( 0, produit );
+    }
+
+    @ParameterizedTest( name = "Verifier somme " )
+    @CsvSource( { "1, 2, 3", "3,3,6" } )
+    public void somme( int argument1, int argument2, int resultat ) {
+
+        assertEquals( resultat, argument1 + argument2 );
+    }
+
+    @Test
+    @Timeout( 1 )
+    public void testLongCalcul() {
+        calculator.longCalcul();
+    }
 
 }
